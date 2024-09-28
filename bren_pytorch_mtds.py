2f4ca26bde438dc5cba35eaed8ab41ff719465cb
JackMa <77jackmack@gmail.com>
2024-09-28 16:37:36 -0700

    Added eval_model() method

--- bren_pytorch_mtds.py
+++ bren_pytorch_mtds.py
@@ -1,4 +1,4 @@
-## This library contains a set of Python methods for training and
+## BREN This library contains a set of Python methods for training and
 ## testing a pytorch model. They require torch and torch.utils.data
 ## to be installed.
 
@@ -56,6 +56,8 @@ def train_loop(model: torch.nn.Module,
     train_acc /= len(data_loader)
     print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
 
+
+
 def test_loop(data_loader: torch.utils.data.DataLoader,
               model: torch.nn.Module,
               loss_fn: torch.nn.Module,
@@ -98,3 +100,32 @@ def test_loop(data_loader: torch.utils.data.DataLoader,
         test_loss /= len(data_loader)
         test_acc /= len(data_loader)
         print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
+
+
+def eval_model(model: torch.nn,Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn = loss_fn,
+               accuracy_fn = accuracy_fn,
+               device = device):
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            #Send data to the GPU
+            X, y = X.to(device), y.to(device)
+
+            # 1.Forward Pass
+            y_pred = Model(X)
+
+            # 2. Calculate loss and accuracy
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true = y,
+                               y_pred = y_pred.argmax(dim = 1)) # Logits -> Labels)
+            
+            # Scale the loss and acc by the number of batches
+            loss /= len(data_loader)
+            acc /= len(data_loader)
+    
+    return{"model_name": model.class_name__,
+           "model_loss": loss.item(),
+           "model_acc": acc}
